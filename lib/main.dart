0e898de1e6a5a9cb7d5a9adc26ed6bd197a6e3cf
saracastro <saritaycloe@hotmail.com>
2025-11-02 17:32:19 -0600

    Actualiza HomeScreen con imagen de internet y textos de bienvenida

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,57 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(fontSize: 24), 
+      title: 'Flutter Widgets Demo',
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to Flutter'),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.network(
+           'https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2021/01/masterclass-cover2.jpg',
+            fit: BoxFit.cover,
           ),
-        ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  '¡Bienvenido a El Catalogo de Peliculas!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Catalogo de Peliculas',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 95, 231, 46),
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 40),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
